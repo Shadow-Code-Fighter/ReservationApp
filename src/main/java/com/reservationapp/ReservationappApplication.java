f0775b87a8901998853dd8524fb928809e4cd3e3
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-06 14:10:10 +0530

    Added Reservation API

--- src/main/java/com/reservationapp/ReservationappApplication.java
+++ src/main/java/com/reservationapp/ReservationappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class ReservationappApplication {
+public class ReservationappApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationappApplication.class, args);
